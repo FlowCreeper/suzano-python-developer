55fca013e4706202c7388fb0154e30761c49fabd
FlowCreeper <caiocoutinho2012@hotmail.com>
2025-04-01 15:05:52 -0300

    Add arithmetic operators module with basic operations and examples

---
+++ Operadores/aritmeticos.py
@@ -0,0 +1,15 @@
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2) # Soma
+print(produto_1 - produto_2) # Subtração
+print(produto_1 / produto_2) # Divisão
+print(produto_1 // produto_2) # Divisão inteira
+print(produto_1 * produto_2) # Multiplicação
+print(produto_1 % produto_2) # Resto da divisão
+print(produto_1 ** produto_2) # Potenciação
+
+x = 10 + 5 * 4 # Em python, a ordem de precedência é a mesma da matemática
+y = (10 / 2) + (25 * 2) - (100 ** 2) # É uma boa prática usar parênteses para deixar o código mais legível
+# O resultado é o mesmo, mas fica mais fácil de entender
+print(x, y)
